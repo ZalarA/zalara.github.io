a8ddbadea6484aa1af0e310008f954480df4ce9b
Aljaž Zalar <45012836+ZalarA@users.noreply.github.com>
2021-11-28 21:56:56 +0100

    Add files via upload

---
+++ trapezno.m
@@ -0,0 +1,19 @@
+function T=trapezno(f,a,b,n)
+
+% Sestavljeno trapezno pravilo
+% Racunanje vrednosti dolocenega integrala
+% Podatki:
+% 		f	zvezna funkcija
+%		a		zacetna tocka intervala
+%		b		koncna tocka intervala
+%		n		stevilo podintervalov
+% Rezultat:
+%		T		priblizek za vrednost integrala
+
+h = (b-a)/n;
+T = (f(a)+f(b))/2;
+   for i = 1:n-1
+      T = T + f(a+i*h);
+   end
+T = T*h;
+
