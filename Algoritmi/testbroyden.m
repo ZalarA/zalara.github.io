b81f7df9f218cac367f5a9236e2ff6188744c4c9
Aljaž Zalar <45012836+ZalarA@users.noreply.github.com>
2022-04-16 21:09:56 +0200

    Add files via upload

--- Algoritmi/testbroyden.m
+++ Algoritmi/testbroyden.m
@@ -1,9 +1,21 @@
-x0=[0.4;0.1;-0.4];
+fprintf(['Resujemo sistem\n'...
+    '   3*x(1) - cos(x(1)*x(2)) - 1.8=0,\n',...
+    '   x(1)^2 - 81*(x(2)+0.1)^2 + sin(x(3)) + 1.1=0\n',...
+    '   exp(-x(1)*x(2)) + 20*x(3) + 9.1=0\n'...
+    'z uporabo Broydenove metode.\n\n'])
 
 F=inline(strcat(...
    '[ 3*x(1) - cos(x(1)*x(2)) - 1.8;',...
    '  x(1)^2 - 81*(x(2)+0.1)^2 + sin(x(3)) + 1.1;',...
    '  exp(-x(1)*x(2)) + 20*x(3) + 9.1]'));
+pause
+
+fprintf(['Zacetni priblizek je x0=[0.4;0.1;-0.4].\n\n'])
+x0=[0.4;0.1;-0.4];
+
+pause
+
+
 
 % Jacobijeva matrika (potrebujemo le za aproksimacijo na zacetku)
 
@@ -13,11 +25,12 @@ JF=inline(strcat(...
    '  -x(2)*exp(-x(1)*x(2)),   -x(1)*exp(-x(1)*x(2)), 20]'));
 
 
-disp('Toèna Jacobijeva matrika v zaèetni toèki');
+disp('Toèna Jacobijeva matrika v zaèetni toèki.');
 JF(x0)
-disp('Za približek vzamemo diag([3,-32,20])')
 
-% Pri Broydenovi metodi na zacetku potrebujemo aproksimacijo Jacobijeve
-% matrike 
+pause
 
-broyden(F,diag([3,-32,20]),[1;1;1]);
+disp('Za približek vzamemo diag([3,-32,20]).')
+
+pause
+broyden(F,eye(3),[1;1;1]);
